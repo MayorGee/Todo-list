beb333143de222e103696952a7fd86f1bbb097d8
mayorCoder <adebayomayowa78@gmail.com>
2022-01-05 10:58:10 +0300

    - I added the delete done item feature - I replaced the joke with a reminder - I updated addTask() to set the display of reminder to none - I updated checked() to add "checked" to the classlist of a done addTask

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 const ul = document.getElementById("my-list");
 const addInput = document.getElementById("new-task");
 const addBtn = document.getElementById("add-btn");
+const body = document.getElementById("body");
+
+
 
 function addTask() {
   let text = addInput.value;
@@ -8,36 +11,63 @@ function addTask() {
   if (text === "") {
     alert("You must type something!");
     return;
+  } else {
+    let paragraph = document.createElement("p");
+    let span = document.createElement("span");
+    let doneBtn = document.createElement("button");
+    let delBtn = document.createElement("button");
+    let li = document.createElement("li");
+
+    li.classList.add("list-item");
+
+    paragraph.innerHTML = text;
+    li.appendChild(paragraph);
+
+    doneBtn.classList.add("done");
+    doneBtn.innerHTML = "Done";
+
+    let att = document.createAttribute("onclick");
+    att.value = "checked(this)";
+    doneBtn.setAttributeNode(att);
+
+    delBtn.classList.add("delete-item");
+    delBtn.innerHTML = "X";
+
+    let attr = document.createAttribute("onclick");
+    attr.value = "deleteItem(this)";
+    delBtn.setAttributeNode(attr);
+
+    span.appendChild(doneBtn);
+    span.appendChild(delBtn);
+
+    li.appendChild(span);
+
+    ul.appendChild(li);
+
+    addInput.value = "";
+
+    document.getElementById("reminder").style.display = "none";
   }
-
-  let li = document.createElement("li");
-  let paragraph = document.createElement("p");
-  let span = document.createElement("span");
-  let button = document.createElement("button");
-
-  paragraph.innerHTML = text;
-  li.appendChild(paragraph);
-
-  button.classList.add("remove");
-  button.innerHTML = "Done";
-  let att = document.createAttribute("onclick");
-  att.value = "checked(this)";
-  button.setAttributeNode(att);
-
-  span.appendChild(button);
-
-  li.appendChild(span);
-
-  ul.appendChild(li);
-
-  addInput.value = "";
-  
 }
+
+addInput.addEventListener("keypress", (e) =>
+  e.key === "Enter" ? addTask() : null
+);
 
 function checked(e) {
- let clickedBtn = e.parentElement;  //span is the immediate parent
- clickedBtn.parentElement.classList.add("checked"); //li is the direct parent of the span
+  let clickedBtn = e.parentElement;
+  let checkedItem = clickedBtn.parentElement;
+
+  checkedItem.classList.add("checked");
 }
 
+function deleteItem(e) {
+  let clickedBtn = e.parentElement;
+  ul.removeChild(clickedBtn.parentElement);
 
-addInput.addEventListener("keypress", ((e) => (e.key === "Enter") ? addTask() : null)); //so that it saves the text once enter is clicked
+  if (ul.children.length == 0 )
+  {
+    document.getElementById("reminder").style.display = "block";
+    
+  }
+}
